452b3ff23dd2fa6606bde1e0872b0e975339fd1b
Florian Sandel <florian@sandel.email>
2023-09-20 10:26:18 +0200

    renmove unnecesarry imports

--- backend/DatabaseQuery.py
+++ backend/DatabaseQuery.py
@@ -1,15 +1,14 @@
-from influxdb_client import InfluxDBClient, Point
-from influxdb_client.client.write_api import SYNCHRONOUS
-from matplotlib import pyplot as plt
+from influxdb_client import InfluxDBClient
 
 import json
+
 def query(*argv,
           url: str = 'http://10.1.70.4:8086/', 
           token: str = 'GBrTQl7C-3RDzIkG_7wNM4hwLPhgweF9CBso-4x6fuvuc1m8I8VxKvwfYckeQqLECm-_BcUAbVjaIZfVAe0pqg==',
           buckets: str = 'plc_dS', 
           org: str = 'Weiss-GmbH',
           timeframe: str = 'start: -10000h'
-          ) -> dict:
+          ):
   client = InfluxDBClient(url=url, token=token, org=org)
   query_api = client.query_api()
   tables = query_api.query(f'from(bucket:"{buckets}") |> range({timeframe})')
