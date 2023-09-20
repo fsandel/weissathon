d3cd791d229fd9c372661781e013391aaf465c12
Florian Sandel <florian@sandel.email>
2023-09-20 14:15:03 +0200

    changed way of query of data and now creating actual json

--- backend/CleanValues.py
+++ backend/CleanValues.py
@@ -1,4 +1,30 @@
 import json
+from DatabaseQuery import query
+# def createJson(allValues: dict, measurements: list) -> json:
+#   return json.dumps(allValues[measurements[0]][1], indent=2)
 
-def createJson(allValues: dict, measurements: list) -> json:
-  return json.dumps(allValues[measurements[0]][1], indent=2)
+GREEN = '1'
+YELLOW = '2'
+RED = '3'
+
+def createJson() -> json:
+  all_entries = query(sensor='kionixX',
+                      measurement='temperature',
+                      field='During_Cycle_Current[A]',
+                      buckets='sensor_dS',
+                      timeframe='start: -30h')
+  # return_json = json()
+  return_json = {}
+  temperature = {}
+  return_json['status'] = GREEN
+  return_json['temperature'] = temperature
+  return_json['temperature']['status'] = GREEN
+  return_json['temperature']['name'] = 'temperature'
+  return_json['temperature']['time'] = all_entries[0]
+  return_json['temperature']['value'] = all_entries[1]
+  return_json['temperature']['description'] = 'this is the temperature of the machine'
+
+
+  return return_json
+
+print(json.dumps(createJson(), indent=2))
--- backend/DatabaseQuery.py
+++ backend/DatabaseQuery.py
@@ -2,25 +2,34 @@ from influxdb_client import InfluxDBClient
 
 import json
 
-def query(*argv,
+def query(sensor: str,
+          measurement: str,
+          field: str,
           url: str = 'http://10.1.70.4:8086/', 
           token: str = 'GBrTQl7C-3RDzIkG_7wNM4hwLPhgweF9CBso-4x6fuvuc1m8I8VxKvwfYckeQqLECm-_BcUAbVjaIZfVAe0pqg==',
-          buckets: str = 'plc_dS', 
+          buckets: str = 'sensor_dS', 
           org: str = 'Weiss-GmbH',
           timeframe: str = 'start: -10000h'
-          ):
+          ) -> tuple:
   client = InfluxDBClient(url=url, token=token, org=org)
   query_api = client.query_api()
   tables = query_api.query(f'from(bucket:"{buckets}") |> range({timeframe})')
   data = json.loads(tables.to_json())
-  return_dict = dict()
-  for arg in argv:
-    time = []
-    value_array = []
-    for entry in data:
-      if entry['_field'] == arg:
+  time = []
+  value_array = []
+  for entry in data:
+    if (buckets.find('sensor') != -1):
+      if measurement == 'vibration' and entry['_measurement'] == measurement:
+        if entry['sensor'] == sensor:
+          if entry['_field'] == field:
+            time.append(entry['_time'])
+            value_array.append(entry['_value'])
+      elif measurement == 'temperature' and entry['_measurement'] == measurement:
         time.append(entry['_time'])
         value_array.append(entry['_value'])
-    tpl = (time, value_array)
-    return_dict.update({arg: tpl})
-  return return_dict
+    elif(buckets.find('plc') != -1):
+        if entry['_field'] == field:
+          time.append(entry['_time'])
+          value_array.append(entry['_value'])
+  return_tpl = (time, value_array)
+  return return_tpl
